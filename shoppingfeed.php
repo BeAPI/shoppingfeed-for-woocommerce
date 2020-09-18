af39d07b56e8839bd331984e5cab4aee5cc67e57
Sami Kerboute <skerboute@beapi.fr>
2020-09-18 16:50:22 +0200

    correct workflows

--- shoppingfeed.php
+++ shoppingfeed.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.shopping-feed.com/
  * Text Domain:     shopping-feed
  * Domain Path:     /languages
- * Version:         6.0.9
+ * Version:         6.0.10
  * Requires at least WP: 5.2
  * Requires at least WooCommerce: 3.8 (3.9/4.0)
  * Requires PHP:      5.6
@@ -25,7 +25,7 @@ if ( file_exists( plugin_dir_path( __FILE__ ) . '/vendor/autoload.php' ) ) {
 	require_once plugin_dir_path( __FILE__ ) . '/vendor/autoload.php';
 }
 
-define( 'SF_VERSION', '6.0.9' );
+define( 'SF_VERSION', '6.0.10' );
 define( 'SF_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'SF_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'SF_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
