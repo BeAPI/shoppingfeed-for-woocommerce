1174f1d3d8dcae3b5f96c7d4a3953090a4a9dec9
Sami Kerboute <skerboute@beapi.fr>
2021-05-26 23:10:41 +0200

    add compat with ats plugin

--- src/ShoppingFeedHelper.php
+++ src/ShoppingFeedHelper.php
@@ -642,18 +642,36 @@ XML;
 
 	/**
 	 * Add filter for order tracking number meta key
+	 *
+	 * @param \WC_Order $wc_order
+	 *
 	 * @return string
 	 */
-	public static function wc_tracking_number() {
-		return apply_filters( 'shopping_feed_tracking_number', '' );
+	public static function wc_tracking_number( $wc_order ) {
+		$tracking_number = apply_filters( 'shopping_feed_tracking_number', '', $wc_order );
+		//COMPACT WITH OLD VERSION
+		if ( '6.0.19' <= SF_VERSION ) {
+			return (string) $wc_order->get_meta( $tracking_number );
+		}
+
+		return $tracking_number;
 	}
 
 	/**
 	 * Add filter for order tracking link meta key
+	 *
+	 * @param \WC_Order $wc_order
+	 *
 	 * @return string
 	 */
-	public static function wc_tracking_link() {
-		return apply_filters( 'shopping_feed_tracking_link', '' );
+	public static function wc_tracking_link( $wc_order ) {
+		$tracking_link = apply_filters( 'shopping_feed_tracking_link', '', $wc_order );
+		//COMPACT WITH OLD VERSION
+		if ( '6.0.19' <= SF_VERSION ) {
+			return (string) $wc_order->get_meta( $tracking_link );
+		}
+
+		return $tracking_link;
 	}
 
 	/**
