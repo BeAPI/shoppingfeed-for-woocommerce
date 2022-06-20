5dd7e9cb86a76155fdb5a5284c61c83f03bb8efc
Clement Boirie <cboirie@beapi.fr>
2022-06-20 11:40:52 +0200

    57779 : Integrate tracking data from WoocommerceShipmentTrackingPro with ShoppingFeed.

--- src/Addons/Addons.php
+++ src/Addons/Addons.php
@@ -9,6 +9,7 @@ use ShoppingFeed\ShoppingFeedWC\Addons\Inventory\Inventory;
 use ShoppingFeed\ShoppingFeedWC\Addons\Marketplaces\Marketplaces;
 use ShoppingFeed\ShoppingFeedWC\Addons\Plugins\ASTPlugin\ASTPlugin;
 use ShoppingFeed\ShoppingFeedWC\Addons\Plugins\ChainedProductsPlugin\ChainedProducts;
+use ShoppingFeed\ShoppingFeedWC\Addons\Plugins\PhWoocommerceShipmentTrackingProPlugin\PhWoocommerceShipmentTrackingProPlugin;
 use ShoppingFeed\ShoppingFeedWC\Addons\Shipping\Shipping;
 
 class Addons {
@@ -26,28 +27,31 @@ class Addons {
 	private $inventory;
 
 	/**
-	 * Shipping class
 	 * @var Marketplaces
 	 */
 	private $marketplaces;
 
 	/**
-	 * Shipping class
 	 * @var ChainedProducts
 	 */
 	private $chained_products_plugin;
 
 	/**
-	 * Shipping class
 	 * @var ASTPlugin
 	 */
 	private $ast_plugin;
 
+	/**
+	 * @var PhWoocommerceShipmentTrackingProPlugin
+	 */
+	private $woocommerce_shipment_tracking_pro;
+
 	public function __construct() {
-		$this->shipping                = new Shipping();
-		$this->inventory               = new Inventory();
-		$this->marketplaces            = new Marketplaces();
-		$this->chained_products_plugin = new ChainedProducts();
-		$this->ast_plugin              = new ASTPlugin();
+		$this->shipping                          = new Shipping();
+		$this->inventory                         = new Inventory();
+		$this->marketplaces                      = new Marketplaces();
+		$this->chained_products_plugin           = new ChainedProducts();
+		$this->ast_plugin                        = new ASTPlugin();
+		$this->woocommerce_shipment_tracking_pro = new PhWoocommerceShipmentTrackingProPlugin();
 	}
 }
