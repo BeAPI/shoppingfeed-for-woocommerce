49a0a469d2a3a95882b85a7718a46ec23b81ad0c
Jérémy Kervran <jkervran@beapi.fr>
2022-08-10 11:53:55 +0200

    Added multiple tracking number handling

--- src/Addons/Plugins/ASTPlugin/ASTPlugin.php
+++ src/Addons/Plugins/ASTPlugin/ASTPlugin.php
@@ -28,9 +28,35 @@ class ASTPlugin {
 	 * @param \WC_Order|\WP_Error $wc_order
 	 */
 	public function get_tracking_number( $tracking_number, $wc_order ) {
-		$tracking_info = $this->get_ast_tracking_data( $wc_order );
+		$tracking_infos = $this->get_ast_tracking_data( $wc_order );
 
-		return empty( $tracking_info['tracking_number'] ) ? $tracking_number : $tracking_info['tracking_number'];
+		// Return default tracking number if AST data is empty
+		if ( ! is_array( $tracking_infos ) || empty( $tracking_infos ) ) {
+			return $tracking_number;
+		}
+
+		// Return first key of AST data if only one set of tracking data
+		if ( count( $tracking_infos ) === 1 ) {
+			$tracking_number_single = $tracking_infos[0]['tracking_number'];
+
+			return empty( $tracking_number_single ) ? $tracking_number : $tracking_number_single;
+		}
+
+		$tracking_numbers = [];
+
+		// Add every tracking number from AST data to array
+		foreach ( $tracking_infos as $info ) {
+			$tracking_numbers[] = $info['tracking_number'];
+		}
+
+		// Implode to comma separated string, depending on PHP version
+		if ( phpversion() >= '7.4.0' ) {
+			$tracking_numbers = implode(',',  $tracking_numbers );
+		} else {
+			$tracking_numbers = implode( $tracking_numbers, ',' );
+		}
+
+		return $tracking_numbers;
 	}
 
 	/**
@@ -53,8 +79,6 @@ class ASTPlugin {
 			$tracking_info = $ast->get_tracking_items( $wc_order->get_id(), true );
 		}
 
-		$tracking_info = end( $tracking_info );
-
 		if ( ! is_array( $tracking_info ) ) {
 			return array();
 		}
@@ -70,6 +94,9 @@ class ASTPlugin {
 	 */
 	public function get_tracking_link( $tracking_link, $wc_order ) {
 		$tracking_info = $this->get_ast_tracking_data( $wc_order );
+
+		// Can me a multidimension array, take only first tracking link of data set
+		$tracking_info = reset( $tracking_info );
 
 		return empty( $tracking_info['formatted_tracking_link'] ) ? $tracking_link : $tracking_info['formatted_tracking_link'];
 	}
