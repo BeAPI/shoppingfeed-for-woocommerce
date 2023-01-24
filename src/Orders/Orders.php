8e7a43b15e7c4081ebe6d681fc90b8282e16b90b
Clement Boirie <cboirie@beapi.fr>
2023-01-24 21:15:12 +0100

    Add new check to control import of orders fulfilled by channel

--- src/Orders/Orders.php
+++ src/Orders/Orders.php
@@ -5,7 +5,9 @@ namespace ShoppingFeed\ShoppingFeedWC\Orders;
 // Exit on direct access
 defined( 'ABSPATH' ) || exit;
 
+use ShoppingFeed\Sdk\Api\Order\OrderResource;
 use ShoppingFeed\Sdk\Api\Store\StoreResource;
+use ShoppingFeed\ShoppingFeedWC\Addons\Marketplace;
 use ShoppingFeed\ShoppingFeedWC\Sdk\Sdk;
 use ShoppingFeed\ShoppingFeedWC\ShoppingFeedHelper;
 
@@ -13,6 +15,8 @@ use ShoppingFeed\ShoppingFeedWC\ShoppingFeedHelper;
  * @psalm-consistent-constructor
  */
 class Orders {
+
+	use Marketplace;
 
 	/**
 	 * @var Orders
@@ -76,8 +80,23 @@ class Orders {
 			if ( Order::exists( $sf_order ) ) {
 				ShoppingFeedHelper::get_logger()->notice(
 					sprintf(
-					/* translators: %1$1s: Order reference. %2$2s: Order id. */
-						__( 'Order already imported  %1$1s => %2$2s', 'shopping-feed' ),
+						/* translators: 1: Order reference. 2: Order id. */
+						__( 'Order already imported %1$1s (%2$2s)', 'shopping-feed' ),
+						$sf_order->getReference(),
+						$sf_order->getId()
+					),
+					array(
+						'source' => 'shopping-feed',
+					)
+				);
+				continue;
+			}
+
+			if ( ! $this->can_import_order( $sf_order ) ) {
+				ShoppingFeedHelper::get_logger()->notice(
+					sprintf(
+						/* translators: 1: Order reference. 2: Order id. */
+						__( 'Order fulfilled by channel %1$1s (%2$2s)', 'shopping-feed' ),
 						$sf_order->getReference(),
 						$sf_order->getId()
 					),
@@ -95,5 +114,22 @@ class Orders {
 		}
 
 		return true;
+	}
+
+	/**
+	 * Check if the order can be imported.
+	 *
+	 * @param OrderResource $sf_order
+	 *
+	 * @return bool
+	 */
+	private function can_import_order( $sf_order ) {
+		// Allow user for force imports for orders fulfilled by the marketplaces.
+		$orders_options = ShoppingFeedHelper::get_sf_orders_options();
+		if ( isset( $orders_options['import_order_fulfilled_by_marketplace'] ) && true === (bool) $orders_options['import_order_fulfilled_by_marketplace'] ) {
+			return true;
+		}
+
+		return false === $this->is_fulfilled_by_marketplace( $sf_order );
 	}
 }
