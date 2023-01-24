e565bbaa67282b9b87cbe083ecfda8592bc08e2d
Clement Boirie <cboirie@beapi.fr>
2023-01-24 20:56:42 +0100

    Update classes to use new helper methods from Marketplace trait

--- src/Addons/Inventory/Marketplaces/MonoMono.php
+++ src/Addons/Inventory/Marketplaces/MonoMono.php
@@ -12,26 +12,18 @@ class MonoMono {
 	use Marketplace;
 
 	public function __construct() {
-		add_action( 'sf_add_metas', array( $this, 'add_metas' ) );
+		add_action( 'sf_add_metas', [ $this, 'add_metas' ] );
 	}
 
 	/**
 	 * @param $metas Metas
 	 */
 	public function add_metas( $metas ) {
-		if (
-			(
-				true !== $this->is_mano_mano( $metas->sf_order ) &&
-				empty( $metas->sf_order_array['additionalFields']['env'] )
-			) ||
-			'epmm' !== $metas->sf_order_array['additionalFields']['env']
-		) {
-			return;
+		if ( $this->is_fulfilled_by_manomano( $metas->sf_order ) ) {
+			$metas->add_meta(
+				Metas::$dont_update_inventory,
+				true
+			);
 		}
-
-		$metas->add_meta(
-			Metas::$dont_update_inventory,
-			true
-		);
 	}
 }
