6b1cc6ec8cde6edaeb79913d4c007c0f33183117
Clement Boirie <cboirie@beapi.fr>
2023-01-24 20:55:01 +0100

    Add new methods to check if an order if fulfilled by the marketplace

--- src/Addons/Marketplace.php
+++ src/Addons/Marketplace.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ShoppingFeed\ShoppingFeedWC\Addons;
 
 use ShoppingFeed\Sdk\Api\Order\OrderResource;
@@ -12,7 +13,7 @@ defined( 'ABSPATH' ) || exit;
  */
 trait Marketplace {
 	/**
-	 * @param $sf_order OrderResource
+	 * @param OrderResource $sf_order
 	 *
 	 * @return bool
 	 */
@@ -24,7 +25,7 @@ trait Marketplace {
 	}
 
 	/**
-	 * @param $sf_order OrderResource
+	 * @param OrderResource $sf_order
 	 *
 	 * @return bool
 	 */
@@ -36,7 +37,7 @@ trait Marketplace {
 	}
 
 	/**
-	 * @param $sf_order OrderResource
+	 * @param OrderResource $sf_order
 	 *
 	 * @return bool
 	 */
@@ -48,13 +49,13 @@ trait Marketplace {
 	}
 
 	/**
-	 * @param $sf_order OrderResource
+	 * @param OrderResource $sf_order
 	 *
 	 * @return bool
 	 */
 	private function is_mano_mano( $sf_order ) {
 		return (
-			strtoupper( $sf_order->getChannel()->getName() ) === 'ManoMano' ||
+			strtoupper( $sf_order->getChannel()->getName() ) === 'MANOMANO' ||
 			$sf_order->getChannel()->getId() === 259
 		);
 	}
@@ -62,10 +63,9 @@ trait Marketplace {
 	/**
 	 * Check if the current SF order is from the Zalando marketplace
 	 *
-	 * @param $sf_order OrderResource
+	 * @param OrderResource $sf_order
 	 *
 	 * @return bool
-	 * @author Stéphane Gillot
 	 */
 	private function is_zalando( $sf_order ) {
 
@@ -82,4 +82,75 @@ trait Marketplace {
 		);
 	}
 
+	/**
+	 * Check if the order is fulfilled by Amazon.
+	 *
+	 * @param OrderResource $sf_order
+	 *
+	 * @return bool
+	 */
+	private function is_fulfilled_by_amazon( $sf_order ) {
+		return $this->is_amazon( $sf_order ) && 'afn' === strtolower( $sf_order->getPaymentInformation()['method'] );
+	}
+
+	/**
+	 * Check if the order is fulfilled by CDiscount.
+	 *
+	 * @param OrderResource $sf_order
+	 *
+	 * @return bool
+	 */
+	private function is_fulfilled_by_cdiscount( $sf_order ) {
+		return $this->is_cdiscount( $sf_order ) && 'clogistique' === strtolower( $sf_order->getPaymentInformation()['method'] );
+	}
+
+	/**
+	 * Check if the order is fulfilled by ManoMano.
+	 *
+	 * @param OrderResource $sf_order
+	 *
+	 * @return bool
+	 */
+	private function is_fulfilled_by_manomano( $sf_order ) {
+		return $this->is_mano_mano( $sf_order ) && 'epmm' === strtolower( $sf_order->toArray()['additionalFields']['env'] );
+	}
+
+	/**
+	 * Check if the order is fulfilled by the channel.
+	 *
+	 * @param OrderResource $sf_order
+	 *
+	 * @return bool
+	 */
+	private function is_fulfilled_by_channel( $sf_order ) {
+		return ! empty( $sf_order->toArray()['fulfilledBy'] ) && 'channel' === strtolower( $sf_order->toArray()['fulfilledBy'] );
+	}
+
+	/**
+	 * Check if the order is fulfilled by a marketplace.
+	 *
+	 * @param OrderResource $sf_order
+	 *
+	 * @return bool
+	 */
+	private function is_fulfilled_by_marketplace( $sf_order ) {
+
+		if ( $this->is_fulfilled_by_amazon( $sf_order ) ) {
+			return true;
+		}
+
+		if ( $this->is_fulfilled_by_cdiscount( $sf_order ) ) {
+			return true;
+		}
+
+		if ( $this->is_fulfilled_by_manomano( $sf_order ) ) {
+			return true;
+		}
+
+		if ( $this->is_fulfilled_by_channel( $sf_order ) ) {
+			return true;
+		}
+
+		return false;
+	}
 }
