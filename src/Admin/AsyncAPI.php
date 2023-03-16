a34d7d8939e2db39fc1e69f29d982220021d7e5a
jeremykervran <32090936+jeremykervran@users.noreply.github.com>
2023-03-16 11:38:41 +0100

    Add pricing & inventory update as async, still wip

--- src/Admin/AsyncAPI.php
+++ src/Admin/AsyncAPI.php
@@ -3,6 +3,10 @@
 namespace ShoppingFeed\ShoppingFeedWC\Admin;
 
 // Exit on direct access
+use ShoppingFeed\Sdk\Api\Catalog\InventoryUpdate;
+use ShoppingFeed\Sdk\Api\Catalog\PricingUpdate;
+use ShoppingFeed\ShoppingFeedWC\ShoppingFeedHelper;
+
 defined( 'ABSPATH' ) || exit;
 
 class AsyncAPI {
@@ -12,11 +16,53 @@ class AsyncAPI {
 		add_action( 'sf_update_product_inventory', [ $this, 'update_product_inventory' ], 10, 2 );
 	}
 
+	/**
+	 * Update products pricings in async mode
+	 *
+	 * @param $pricing_api
+	 * @param $pricing_update
+	 *
+	 * @return void
+	 */
 	public function update_product_pricing( $pricing_api, $pricing_update ) {
+		if ( ! $pricing_update instanceof PricingUpdate ) {
+			ShoppingFeedHelper::get_logger()->error(
+				sprintf(
+					__( 'pricing_update variable not instance of PricingUpdate class', 'shopping-feed' )
+				),
+				[
+					'source' => 'shopping-feed',
+				]
+			);
+
+			return;
+		}
+
 		$pricing_api->execute( $pricing_update );
 	}
 
+	/**
+	 * Update products inventory in async mode
+	 *
+	 * @param $inventory_api
+	 * @param $inventory_update
+	 *
+	 * @return void
+	 */
 	public function update_product_inventory( $inventory_api, $inventory_update ) {
+		if ( ! $inventory_update instanceof InventoryUpdate ) {
+			ShoppingFeedHelper::get_logger()->error(
+				sprintf(
+					__( 'inventory_update variable not instance of InventoryUpdate class', 'shopping-feed' )
+				),
+				[
+					'source' => 'shopping-feed',
+				]
+			);
+
+			return;
+		}
+
 		$inventory_api->execute( $inventory_update );
 	}
 
