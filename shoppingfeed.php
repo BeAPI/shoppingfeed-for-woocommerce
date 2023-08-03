00541619113327fa74d3013d9ad26c93c4808504
Clement Boirie <cboirie@beapi.fr>
2023-08-03 15:30:50 +0200

    chore: prepare release 6.2.0

--- shoppingfeed.php
+++ shoppingfeed.php
@@ -7,7 +7,7 @@
  * Author URI:           https://www.shopping-feed.com/
  * Text Domain:          shopping-feed
  * Domain Path:          /languages
- * Version:              6.1.19
+ * Version:              6.2.0
  * Requires at least:    5.7
  * Requires PHP:         7.1
  * WC requires at least: 5.1.0
@@ -26,7 +26,7 @@ if ( file_exists( plugin_dir_path( __FILE__ ) . '/vendor/autoload.php' ) ) {
 	require_once plugin_dir_path( __FILE__ ) . '/vendor/autoload.php';
 }
 
-define( 'SF_VERSION', '6.1.19' );
+define( 'SF_VERSION', '6.2.0' );
 define( 'SF_DB_VERSION_SLUG', 'SF_DB_VERSION' );
 define( 'SF_DB_VERSION', '1.0.0' );
 define( 'SF_UPGRADE_RUNNING', 'SF_UPGRADE_RUNNING' );
