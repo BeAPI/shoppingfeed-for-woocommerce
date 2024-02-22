8aaa6fd9a1afe0f517281113b0e4e55f3c8bf98b
Clement Boirie <cboirie@beapi.fr>
2024-02-22 01:24:17 +0100

    Woocommerce HPOS compatibility
    Co-authored-by: sgillot <sgillot@beapi.fr>

---
+++ tests/wpunit/Order/OrderImportTestCase.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ShoppingFeed\ShoppingFeedWC\Tests\wpunit\Order;
+
+use ShoppingFeed\ShoppingFeedWC\Dependencies\ShoppingFeed\Sdk\Api\Order\OrderResource;
+use ShoppingFeed\ShoppingFeedWC\Dependencies\ShoppingFeed\Sdk\Hal\HalClient;
+use ShoppingFeed\ShoppingFeedWC\Dependencies\ShoppingFeed\Sdk\Hal\HalResource;
+
+class OrderImportTestCase extends \Codeception\TestCase\WPTestCase {
+
+	protected function get_order_resource( string $name ): OrderResource {
+		$order_id  = random_int( 10000000000, 90000000000 );
+		$order_ref = uniqid( 'TEST-' );
+		codecept_debug( $order_id );
+		codecept_debug( $order_ref );
+
+		$raw         = str_replace(
+			[ '%order_id%', '%order_reference%' ],
+			[ $order_id, $order_ref ],
+			file_get_contents( __DIR__ . '/data/' . $name . '.json' )
+		);
+		$json        = json_decode( $raw, true );
+		$client      = $this->createMock( HalClient::class );
+		$halResource = $this
+			->getMockBuilder( HalResource::class )
+			->setConstructorArgs( [ $client, $json, $json['_links'], $json['_embedded'] ] )
+			->setMethods( [ 'getFirstResources' ] )
+			->getMock();
+
+		return new OrderResource( $halResource );
+	}
+
+	protected function get_default_order_status(): string {
+		$status = \ShoppingFeed\ShoppingFeedWC\ShoppingFeedHelper::get_sf_default_order_status();
+		if ( str_starts_with( $status, 'wc-' ) ) {
+			$status = substr( $status, 3 );
+		}
+
+		return $status;
+	}
+
+	protected function get_error_order_status(): string {
+		return 'failed';
+	}
+}
