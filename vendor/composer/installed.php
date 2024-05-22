01aee985360ac541e821aa689f768853020796a4
Clement Boirie <cboirie@beapi.fr>
2024-05-22 12:29:04 +0000

    Release version 6.6.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '87a5dc6149fa466ede1db9e1e2684ce0f46012f6',
+        'name' => 'shoppingfeed/shoppingfeed-for-woocommerce',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'dev_requirement' => false,
+        ),
+        'shoppingfeed/php-feed-generator' => array(
+            'pretty_version' => '1.2.5',
+            'version' => '1.2.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../shoppingfeed/php-feed-generator',
+            'aliases' => array(),
+            'reference' => 'e2f89cdb804f16bbc282b7da0a8bac226fae6cc7',
+            'dev_requirement' => false,
+        ),
+        'shoppingfeed/shoppingfeed-for-woocommerce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '87a5dc6149fa466ede1db9e1e2684ce0f46012f6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
