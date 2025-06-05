5ba845ae6687fcd90023fa957cb019e3a9a65921
Clement Boirie <cboirie@beapi.fr>
2025-06-05 10:07:23 +0200

    improve logs during orders import process

--- src/Orders/Orders.php
+++ src/Orders/Orders.php
@@ -54,19 +54,24 @@ class Orders {
 	 * Get Orders from SF
 	 */
 	public function get_orders( $sf_account, $since = '' ) {
+		ShoppingFeedHelper::log(
+			\WC_Log_Levels::INFO,
+			'>>> Starting orders import process >>>',
+			'shopping-feed-orders'
+		);
+
 		$shop = Sdk::get_sf_shop( $sf_account );
 
 		if ( ! $shop instanceof StoreResource ) {
-			ShoppingFeedHelper::get_logger()->error(
+			ShoppingFeedHelper::log(
+				\WC_Log_Levels::ERROR,
 				sprintf(
-					/* translators: %s: Error message. */
-					__( 'Cannot retrieve shop from SDK for account : %s', 'shopping-feed' ),
+					'Cannot retrieve shop from SDK for account : %s',
 					$sf_account['username']
 				),
-				array(
-					'source' => 'shopping-feed',
-				)
+				'shopping-feed-orders'
 			);
+
 			return false;
 		}
 
@@ -81,48 +86,97 @@ class Orders {
 		$orders_options               = ShoppingFeedHelper::get_sf_orders_options();
 		$include_fulfilled_by_channel = (bool) ( $orders_options['import_order_fulfilled_by_marketplace'] ?? false );
 
+		ShoppingFeedHelper::log(
+			\WC_Log_Levels::DEBUG,
+			'Fetching orders from API',
+			'shopping-feed-orders',
+			[
+				'filters' => $filters,
+			]
+		);
+
 		foreach ( $order_api->getAll( $filters ) as $sf_order ) {
+			ShoppingFeedHelper::log(
+				\WC_Log_Levels::INFO,
+				sprintf( '[Order %s] Start processing.', $sf_order->getReference() ),
+				'shopping-feed-orders',
+				[
+					'id' => $sf_order->getId(),
+					'channel' => $sf_order->getChannel()->getName(),
+					'status' => $sf_order->getStatus(),
+				]
+			);
+
 			if ( Order::exists( $sf_order ) ) {
-				ShoppingFeedHelper::get_logger()->notice(
-					sprintf(
-						/* translators: 1: Order reference. 2: Order id. */
-						__( 'Order already imported %1$s (%2$s)', 'shopping-feed' ),
-						$sf_order->getReference(),
-						$sf_order->getId()
-					),
-					array(
-						'source' => 'shopping-feed',
-					)
+				$existing_order = Order::find_by_sf_reference( $sf_order->getReference() );
+				ShoppingFeedHelper::log(
+					\WC_Log_Levels::INFO,
+					sprintf( '[Order %s] Order already imported.', $sf_order->getReference() ),
+					'shopping-feed-orders',
+					[
+						'wc_order' => $existing_order->get_id(),
+					]
 				);
+
 				continue;
 			}
 
 			$import_order_check = $this->can_import_order( $sf_order, $include_fulfilled_by_channel );
 			if ( is_wp_error( $import_order_check ) ) {
-				/** @psalm-suppress PossiblyInvalidMethodCall */
-				ShoppingFeedHelper::get_logger()->notice(
+				ShoppingFeedHelper::log(
+					\WC_Log_Levels::NOTICE,
 					sprintf(
-						/* translators: 1: Order reference, 2: Order id, 3: error message. */
-						__( 'Order %1$s (%2$s) can\'t be imported : %3$s', 'shopping-feed' ),
+						"[Order %s] Order can't be imported : %s",
 						$sf_order->getReference(),
-						$sf_order->getId(),
 						$import_order_check->get_error_message()
 					),
-					array(
-						'source' => 'shopping-feed',
-					)
+					'shopping-feed-orders'
 				);
+
 				continue;
 			}
 
 			// Include VAT when importing the order.
 			$include_vat = (bool) ( ShoppingFeedHelper::get_sf_orders_options()['import_vat_order'] ?? false );
+			ShoppingFeedHelper::log(
+				\WC_Log_Levels::DEBUG,
+				sprintf(
+					'[Order %s] Will include VAT when importing the order : %s',
+					$sf_order->getReference(),
+					( $include_vat ) ? 'yes' : 'no'
+				),
+				'shopping-feed-orders'
+			);
 
 			//Init Order
 			$order = new Order( $sf_order, $include_vat );
-			//Add Order
-			$order->add();
+			try {
+				//Add Order
+				$order->add();
+			} catch ( \Exception $e ) {
+				ShoppingFeedHelper::log(
+					\WC_Log_Levels::CRITICAL,
+					sprintf(
+						'[Order %s] Import process exception : %s',
+						$sf_order->getReference(),
+						$e->getMessage()
+					),
+					'shopping-feed-orders'
+				);
+			}
+
+			ShoppingFeedHelper::log(
+				\WC_Log_Levels::INFO,
+				sprintf( '[Order %s] Processing completed.', $sf_order->getReference() ),
+				'shopping-feed-orders'
+			);
 		}
+
+		ShoppingFeedHelper::log(
+			\WC_Log_Levels::INFO,
+			'<<< Orders import process complete <<<',
+			'shopping-feed-orders'
+		);
 
 		return true;
 	}
